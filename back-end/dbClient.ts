6ad7652a11dbe24da885ea1f3dd139c16b710175
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-25 16:38:07 +0300

    add db client

---
+++ back-end/dbClient.ts
@@ -0,0 +1,14 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  }
+});
+
+export default client;
