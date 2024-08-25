e907d8e2ebb343b751b817257a011b1e995ca1a1
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-25 18:30:53 +0300

    data seeding done

--- back-end/src/insertData.js
+++ back-end/src/insertData.js
@@ -1,47 +1,52 @@
 import {  PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import client from "./dbClient.js";
+import fs from 'fs';
 
 
 
-const data = [
-  {
-    Call:{
-      CallID: "call-001",
-      Received: "2024-08-24T08:00:00Z",
-      Accepted: true,
-      HandledByAgent: "AI",
-      RedirectedToAgent: "Human",
-      Length: 300,
-      Outcome: "Led to sale",
-      Topic: "Oil Change",
-      CallTranscript: "Customer requested an oil change...",
-      Feedback: {
-        HappyWithAI: true,
-        HappyWithHuman: true,
-        WillUseServiceAgain: true,
-        FeedbackTime: "2024-08-24T08:10:00Z"
-      }
-    },
-    Caller: {
-      CallerID: "caller-001",
-      Name: "John Doe",
-      PhoneNumber: "+123456789",
-      Location: "New York",
-      IsPreviousCustomer: true,
-      TimeOfCall: "2024-08-24T08:00:00Z",
-      Language: "English",
-      CallHistory: ["call-001"]
-    },
-    Agent: {
-      AgentID: "agent-001",
-      AgentType: "AI",
-      Model: "v1.0",
-      Performance: "High"
-    }
-  },
+// const data = [
+//   {
+//     Call:{
+//       CallID: "call-001",
+//       Received: "2024-08-24T08:00:00Z",
+//       Accepted: true,
+//       HandledByAgent: "AI",
+//       RedirectedToAgent: "Human",
+//       Length: 300,
+//       Outcome: "Led to sale",
+//       Topic: "Oil Change",
+//       CallTranscript: "Customer requested an oil change...",
+//       Feedback: {
+//         HappyWithAI: true,
+//         HappyWithHuman: true,
+//         WillUseServiceAgain: true,
+//         FeedbackTime: "2024-08-24T08:10:00Z"
+//       }
+//     },
+//     Caller: {
+//       CallerID: "caller-001",
+//       Name: "John Doe",
+//       PhoneNumber: "+123456789",
+//       Location: "New York",
+//       IsPreviousCustomer: true,
+//       TimeOfCall: "2024-08-24T08:00:00Z",
+//       Language: "English",
+//       CallHistory: ["call-001"]
+//     },
+//     Agent: {
+//       AgentID: "agent-001",
+//       AgentType: "AI",
+//       Model: "v1.0",
+//       Performance: "High"
+//     }
+//   },
 
-];
+// ];
+
+const dataPath =  '../generated_call_data.json';
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
 
 async function insertData() {
   for (const item of data) {
